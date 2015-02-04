092ad56ca77da8ebb1472241eb384b39da68fb81
palashjohari <palashjohari@gmail.com>
2015-02-05 01:49:49 +0530

    first commit

---
+++ static/oscar/js/parallax.js
@@ -0,0 +1,86 @@
+(function($) {
+
+    $.fn.parallax = function(options) {
+
+        var windowHeight = $(window).height();
+
+        // Establish default settings
+        var settings = $.extend({
+            speed        : 0.15
+        }, options);
+
+        // Iterate over each object in collection
+        return this.each( function() {
+
+        	// Save a reference to the element
+        	var $this = $(this);
+
+        	// Set up Scroll Handler
+        	$(document).scroll(function(){
+
+    		        var scrollTop = $(window).scrollTop();
+            	        var offset = $this.offset().top;
+            	        var height = $this.outerHeight();
+
+    		// Check if above or below viewport
+			if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+				return;
+			}
+
+			var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+
+                 // Apply the Y Background Position to Set the Parallax Effect
+    			$this.css('background-position', 'center ' + yBgPosition + 'px');
+
+        	});
+        });
+    }
+}(jQuery));
+
+$('.bg-1,.bg-3').parallax({
+	speed :	0.15
+});
+
+$('.bg-2').parallax({
+	speed :	0.25
+});
+
+
+$('.bg-1,.bg-3').parallax({
+	speed :	0.15
+});
+
+$('.bg-2').parallax({
+	speed :	0.25
+});
+
+
+
+/* affix the navbar after scroll below header */
+$('#nav').affix({
+      offset: {
+        top: $('.page_inner').height()
+      }
+});
+
+/* highlight the top nav as scrolling occurs */
+$('body').scrollspy({ target: '#nav' })
+
+/* smooth scrolling for scroll to top */
+$('.scroll-top').click(function(){
+  $('body,html').animate({scrollTop:0},1000);
+})
+
+/* smooth scrolling for nav sections */
+$('#nav .navbar-nav li>a').click(function(){
+  var link = $(this).attr('href');
+  var posi = $(link).offset().top;
+  $('body,html').animate({scrollTop:posi},700);
+});
+
+
+
+
+
+
+
